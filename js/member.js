161df8ccdd9866c2b87ae913609356c88d839908
JoeDeeK <jody2be@yahoo.ca>
2016-11-26 10:50:31 -0500

    Button listener and information display
    Retrieved info from .json to display on member pages. Added listener to buttons to control which member information is displayed.

--- js/member.js
+++ js/member.js
@@ -1,8 +1,8 @@
 "use strict";
+var num;
 
 // data object : data.member
 function initMember(json) {
-
     data.member = json;
     console.log("init-member", json);
 }
@@ -12,6 +12,13 @@ function initMember(json) {
  *
  * @param item used for member-details page
  */
-function initMemberDetails(item) {
+function initMemberDetails(item) {	
+	$(".nav-member").click(function(){
+		num = $(this).attr("value");
+		$("#sName").html(data.member.member[num].fullName);
+		$("#sLog").html(data.member.member[num].studentLogin);
+		$("#sNum").html(data.member.member[num].studentNumber);
+		$("#memberPic").html("<img src='" +  data.member.member[num].studentPicture + "' height='50%' width='50%'>");			
+	});   
     console.log("init member-details", item);
 }
