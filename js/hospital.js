62cfc9dec4f93d0f7e573bf0a403284e918b3502
serraon <nicoleserrao25@gmail.com>
2016-11-30 19:10:33 -0500

    Add files via upload

--- js/hospital.js
+++ js/hospital.js
@@ -1,5 +1,20 @@
 "use strict";
 
+//globals
+var lngHosp;
+var latHosp;
+var mapDesc;
+var mapHosp;
+var mapOptions;
+var map;
+var myLoc;
+var info;
+
+
+var latTO;
+var lngTO;
+
+
 // data object : data.hospital
 
 function initHospital(json) {
@@ -20,13 +35,57 @@ function initHospital(json) {
       "Phone Number: " + json.hospital.mainPhoneNumber + "<br>" +
       "Longitude: " + json.hospital.longitude +
       "&nbsp; &nbsp; Latitude: " + json.hospital.latitude
-    );
+    );    
+    
+
+    $('#mapSection').show();
+
+    //map description
+    $('#mapSection').show();
+
+    //coordinates
+    lngHosp = json.hospital.longitude;
+    latHosp = json.hospital.latitude;
+    
+    //first step is to set center point
+    mapHosp = new google.maps.LatLng(latHosp, lngHosp);
+    
+    //set map options
+    mapOptions = {
+        center: mapHosp, 
+        zoom:18,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+        //other options are roadmap, salellite, terain
+    }
+    
+    //draw map
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+                        
+    //marker
+    myLoc = new google.maps.Marker({
+        map: map,
+        icon: "_img/pushpin.gif",    //optional ... without will get default
+        animation: google.maps.Animation.DROP,
+        position: mapHosp
+    });
+         
+    //info window content
+    info = new google.maps.InfoWindow({
+        content: 'test'
+    });
+    
+    //add listener
+    google.maps.event.addListener(myLoc, "click", function() {
+       info.open(map, myLoc); 
+    }); 
+
+    
+    
     
     //departments  summHosp
     var depart = json.hospital.departments;
     
     for(var x=0; x < depart.length; x++){
-        console.log(depart[x].departmentName );
         $("#summHosp").append(
             "<section id='summDepart" + x +"' data-role='collapsible'>" +
                 "<h3>" + depart[x].departmentName + "</h3>" +
@@ -45,6 +104,7 @@ function initHospital(json) {
 
 }
 
+
 /**
  * Initialize hospital-details page When an item is clicked
  *
@@ -52,7 +112,5 @@ function initHospital(json) {
  */
 function initHospitalDetails(item) {
 
-   // console.log("init-hospital-details", item);
-   console.log("test ------" + item);
     //$("#hospName").html(item.name);
 }
