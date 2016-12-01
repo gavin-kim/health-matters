e1df9bc7bae2ce589af60f62990ac3d333a32a7b
serraon <nicoleserrao25@gmail.com>
2016-12-01 01:35:31 -0500

    Add files via upload
    map updates

--- js/hospital.js
+++ js/hospital.js
@@ -3,17 +3,22 @@
 //globals
 var lngHosp;
 var latHosp;
+var lngSH;
+var latSH;
+var mapCampus;
+var myPath;
+var pathCoordinates;
+
 var mapDesc;
 var mapHosp;
 var mapOptions;
 var map;
 var myLoc;
+var myLoc2;
 var info;
+var info2;
 
-
-var latTO;
-var lngTO;
-
+var depart;
 
 // data object : data.hospital
 
@@ -50,7 +55,7 @@ function initHospitalDetails(hospital) {
 
 
     //departments  summHosp
-    var depart = hospital.departments;
+    depart = hospital.departments;
 
     // reset html elements
     $("#summHosp").html("");
@@ -85,17 +90,17 @@ function initMap() {
     //map description
     $('#mapSection').show();
 
-    //coordinates
-    lngHosp = data.hospital.longitude;
-    latHosp = data.hospital.latitude;
+    //coordinates for hospital
+    lngHosp = parseFloat(data.hospital.longitude);
+    latHosp = parseFloat(data.hospital.latitude);
 
     //first step is to set center point
     mapHosp = new google.maps.LatLng(latHosp, lngHosp);
 
-    //set map options
+    //set map - points to hospital
     mapOptions = {
         center: mapHosp,
-        zoom:18,
+        zoom:9,
         mapTypeId: google.maps.MapTypeId.HYBRID
         //other options are roadmap, salellite, terain
     };
@@ -103,21 +108,67 @@ function initMap() {
     //draw map
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-    //marker
+    //marker for hospital
     myLoc = new google.maps.Marker({
         map: map,
-        //icon: "_img/pushpin.gif",    //optional ... without will get default
+        icon: "_img/pushpin.gif",    //optional ... without will get default
         animation: google.maps.Animation.DROP,
         position: mapHosp
-    });
+    }); 
 
     //info window content
     info = new google.maps.InfoWindow({
-        content: 'test'
+        content: data.hospital.name
     });
 
-    //add listener
+    //add listener 
     google.maps.event.addListener(myLoc, "click", function() {
         info.open(map, myLoc);
     });
+       
+    //Sheridan Campus Coordinates (trafalgar)
+    latSH = 43.469280;
+    lngSH = -79.698683;
+    
+    //info for second marker
+    var locNameSH = 'Sheridan Trafalgar Campus';
+    var grpNames = 'Yoonkwan Kim, Jody Kirton, Nicole Serrao';
+   
+    // coordinates on map for Sheridan
+    mapCampus = new google.maps.LatLng(latSH, lngSH);
+    
+    //marker for sheridan campus
+    myLoc2 = new google.maps.Marker({
+        map: map,
+        icon: "_img/pushpin.gif",			//optional ... without will get default
+        animation: google.maps.Animation.DROP,		// or BOUNCE (optional)
+        position: mapCampus
+    });
+    
+    //info window content - sheridan campus
+    info2 = new google.maps.InfoWindow({
+        content: locNameSH + "<br>" + grpNames
+    });
+
+    //add listener for information for marker 
+    google.maps.event.addListener(myLoc2, "click", function() {
+        info2.open(map, myLoc2);
+    });
+        	
+    //array of points for polyline
+    pathCoordinates = [
+        {lat: latHosp, lng: lngHosp},
+        {lat: latSH, lng: lngSH}
+    ];
+    
+    //polyline
+    myPath = new google.maps.Polyline ({
+        path: pathCoordinates,
+        strokeColor: "#ff0000",
+        strokeOpacity: 1.0,			// 0-1 transparency
+        strokeWeight: 2
+    });
+    
+    //set polyline on map
+    myPath.setMap(map);
 }
